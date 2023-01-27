8c26e21d0cece70065c66a47cd58d18ef4b8d03d
amir <amirazizihosein@gmail.com>
2023-01-27 19:11:13 +0330

    update index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html}", "./js/*.{js}"],
+  content: ["./*.html"],
   theme: {
     extend: {
       screens: {
@@ -8,6 +8,13 @@ module.exports = {
         md: "768px",
         lg: "976px",
         lx: "1440px",
+      },
+      colors: {
+        //  Primary
+        cl_DesaturatedRed: "hsl(0, 36%, 70%)",
+        cl_SoftRed: "hsl(0, 93%, 68%)",
+        // Neutral
+        cl_DarkGrayishRed: "hsl(0, 6%, 24%)",
       },
     },
   },
